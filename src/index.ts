b07e015b9eaa07e453b90cd3ef7c855ebdc35bfc
Lam, Doris T (319E) <Doris.T.Lam@jpl.nasa.gov>
2020-08-31 10:30:53 -0700

    log some stuff, add mms id to new cells

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-import { requestAPI } from './jupyterlab-mms';
+//import { requestAPI } from './jupyterlab-mms';
+import { INotebookTracker } from '@jupyterlab/notebook';
+import { UUID } from '@lumino/coreutils';
 
 /**
  * Initialization data for the jupyterlab-mms extension.
@@ -11,8 +13,39 @@ import { requestAPI } from './jupyterlab-mms';
 const extension: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab-mms',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
+  requires: [INotebookTracker],
+  activate: (app: JupyterFrontEnd, notebookTracker: INotebookTracker) => {
     console.log('JupyterLab extension jupyterlab-mms is activated!');
+    notebookTracker.widgetAdded.connect((sender, panel) => {
+      console.log('widgetAdded'); //notebook metadata is not initialized yet - has no mms key
+      panel.content.modelChanged.connect((notebook, none) => {
+        console.log('content model changed'); //never fires??
+      });
+      panel.content.modelContentChanged.connect((notebook, none) => {
+        console.log('content modelContent changed'); //on close notebook.model becomes null
+      });
+      panel.model.contentChanged.connect((notebookModel, none) => {
+        console.log('model content changed'); //on close notebookModel.cells becomes null, seems fires exactly the same as above
+      });
+      panel.model.stateChanged.connect((notebookModel, changed) => {
+        console.log('model state changed'); //when notebook is fully opened - changed is {name: 'dirty', newValue: false, oldValue: true} - vice versa
+      });
+      //if (panel.content.model.metadata.has('mms')) {
+        console.log('found mms notebook');
+        panel.content.model.cells.changed.connect((cells, changed) => {
+          console.log('cells changed');
+          for (const cell of changed.newValues.values()) {
+            if (!cell.metadata.has('mms')) {
+              const uid = UUID.uuid4();
+              console.log('adding mms cell id ' + uid);
+              cell.metadata.set('mms', { id: uid });
+            }
+          }
+        });
+      //}
+    });
+
+    /*
     requestAPI<any>('get_example')
       .then(data => {
         console.log(data);
@@ -22,6 +55,7 @@ const extension: JupyterFrontEndPlugin<void> = {
           `The jupyterlab_mms server extension appears to be missing.\n${reason}`
         );
       });
+      */
   }
 };
 
