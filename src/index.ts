d9ab659aca24e7f524ae59a202d17f80fd6116a1
Lam, Doris T (319E) <Doris.T.Lam@jpl.nasa.gov>
2020-09-01 01:25:08 -0700

    add buttons to simulate cell updates

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,17 @@ import { DocumentRegistry } from '@jupyterlab/docregistry';
 import { IDisposable } from '@lumino/disposable';
 import { Widget } from '@lumino/widgets';
 import { IChangedArgs } from '@jupyterlab/coreutils';
-import { ICellModel } from '@jupyterlab/cells';
+import {
+  CodeCellModel,
+  ICellModel,
+  MarkdownCellModel,
+  RawCellModel
+} from '@jupyterlab/cells';
 import { IObservableUndoableList } from '@jupyterlab/observables';
 import { IObservableList } from '@jupyterlab/observables/lib/observablelist';
 import { ToolbarButton } from '@jupyterlab/apputils';
+//import IContentFactory = NotebookModel.IContentFactory;
+//import { IBaseCell, ICodeCell } from '@jupyterlab/nbformat';
 
 /**
  * Initialization data for the jupyterlab-mms extension.
@@ -50,42 +57,50 @@ class MmsNotebookWidgetExtension
   ): IDisposable {
     const widget = new MmsNotebookWidget(panel, context);
 
-    const callback = () => {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-      // @ts-ignore
-      const json = {
-        metadata: {
-          mms: {
-            id: '7de84b71-d244-482a-9976-7f99d69b81ab'
-          }
-        },
-        _modifier: 'admin',
-        _docId: '8f902519-a6e9-4aae-be69-7c6aee2c2c30',
-        source: 'some python code here for testing',
-        _commitId: 'b1168114-e631-4909-8708-3df98c280b65',
-        _inRefIds: ['master'],
-        _creator: 'admin',
-        // eslint-disable-next-line @typescript-eslint/camelcase
-        _created: '2020-08-31T12:11:43.118-0700',
-        id: '7de84b71-d244-482a-9976-7f99d69b81ab',
-        // eslint-disable-next-line @typescript-eslint/camelcase
-        cell_type: 'code',
-        _refId: 'master',
-        _modified: '2020-08-31T12:11:43.118-0700',
-        _projectId: 'jupyter2'
-      };
-      console.log('dummy test a cell update');
-      widget.mmsTestCellUpdate(json);
+    const testUpdateJson = {
+      metadata: {
+        mms: {
+          id: '7de84b71-d244-482a-9976-7f99d69b81ab'
+        }
+      },
+      source: 'some python code here for testing',
+      id: '7de84b71-d244-482a-9976-7f99d69b81ab',
+      // eslint-disable-next-line @typescript-eslint/camelcase
+      cell_type: 'code'
     };
-    const button = new ToolbarButton({
+    const button1 = new ToolbarButton({
       className: 'myButton',
       iconClass: 'fa fa-fast-forward',
-      onClick: callback,
+      onClick: () => {
+        console.log('dummy test a cell update');
+        widget.mmsCellUpdate(testUpdateJson);
+      },
       tooltip: 'Test mms cell update'
     });
+    const button2 = new ToolbarButton({
+      className: 'myButton',
+      iconClass: 'fa fa-fast-forward',
+      onClick: () => {
+        const uid = UUID.uuid4();
+        const testAddJson = {
+          metadata: {
+            mms: {
+              id: uid
+            }
+          },
+          source: 'added cell',
+          id: uid,
+          // eslint-disable-next-line @typescript-eslint/camelcase
+          cell_type: 'code'
+        };
+        console.log('dummy test a cell add');
+        widget.mmsCellAdd(testAddJson, 0);
+      },
+      tooltip: 'Test mms cell add'
+    });
 
-    panel.toolbar.insertItem(0, 'testMmsCellUpdate', button);
-
+    panel.toolbar.insertItem(0, 'testMmsCellUpdate', button1);
+    panel.toolbar.insertItem(1, 'testMmsCellAdd', button2);
     return widget;
   }
 }
@@ -152,25 +167,54 @@ class MmsNotebookWidget extends Widget {
       }
     }
     if ('remove' === changed.type) {
-      console.log('cell removed');
       for (const cell of changed.oldValues.values()) {
+        cell.contentChanged.disconnect(this._cellChanged, this);
         const mmsId = (cell.metadata.toJSON().mms as PartialJSONObject)
           .id as string;
+        console.log('cell removed ' + mmsId);
         this._cellsMap.delete(mmsId);
       }
+    }
+    if ('set' === changed.type) {
+      console.log('cell set?');
+    }
+    if ('move' === changed.type) {
+      console.log('cell moved');
     }
     //send/update mms notebook cell references?
   }
 
   private _cellChanged(cellModel: ICellModel) {
-    console.log('cell content changed');
+    console.log('cell content changed ' + cellModel.value.text);
   }
 
-  public mmsTestCellUpdate(json: any): void {
-    console.log('simulate update first code cell');
+  public mmsCellUpdate(json: any): void {
+    console.log('simulate update first code cell ' + json);
     const cell = this._panel.model.cells.get(0);
     cell.value.text = json.source;
     //will need to handle cell type changes, etc, see @jupyterlab/cells models.ts
+  }
+
+  public mmsCellRemove(id: string): void {
+    console.log('simulate remove cell ' + id);
+    if (this._cellsMap.has(id)) {
+      const cell = this._cellsMap.get(id);
+      this._panel.model.cells.removeValue(cell);
+      this._cellsMap.delete(id);
+    }
+  }
+
+  public mmsCellAdd(json: any, index: number): void {
+    console.log('simulate add cell ' + json);
+    let cellModel: ICellModel = null;
+    if (json.cell_type === 'code') {
+      cellModel = new CodeCellModel({ cell: json });
+    } else if (json.cell_type === 'markdown') {
+      cellModel = new MarkdownCellModel({ cell: json });
+    } else {
+      cellModel = new RawCellModel({ cell: json });
+    }
+    this._panel.model.cells.insert(index, cellModel);
   }
 }
 export default extension;
